f3b4c3419c0e6bc85b4d2aa6677a375c84e8cb8a
Darwison <daorjuela5@poligran.edu.co>
2019-10-14 00:44:19 +0200

    ajustes

--- src/app/services/swapi.service.ts
+++ src/app/services/swapi.service.ts
@@ -14,15 +14,13 @@ export class SwapiService {
   getPeople() {
     return this.http.get('https://swapi.co/api/people/');
   }
-<<<<<<< HEAD
   getPlanet() {
     return this.http.get('https://swapi.co/api/planets/');
-=======
+  }
   getVehicle() {
     return this.http.get('https://swapi.co/api/vehicles/');
   }
   getStarship() {
     return this.http.get('https://swapi.co/api/starships/');
->>>>>>> da69ddc7bb5395c25cf82f8d6dd2111b30b7237e
   }
 }
