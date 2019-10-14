a71ec4f84d99be80323b5a7d4a21781c24828da1
Darwison <daorjuela5@poligran.edu.co>
2019-10-14 18:32:44 +0200

    Loading Planetas

--- src/app/components/planetas/planetas.component.ts
+++ src/app/components/planetas/planetas.component.ts
@@ -9,13 +9,19 @@ import { SwapiService } from '../../services/swapi.service';
 export class PlanetasComponent implements OnInit {
 
   planetas: any[] = [];
+  loading: boolean;
   constructor(private swapi: SwapiService) {
+    this.loading = true;
     this.swapi.getCall('planets')
       .subscribe( (data: any) => {
+        console.log(data);
         this.planetas = data;
+        this.loading = false;
       });
   }
-
+  verPlaneta(item: any) {
+    console.log(item);
+  } 
   ngOnInit() {
   }
 
