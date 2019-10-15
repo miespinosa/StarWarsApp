95effe294e628e15d42695aae0208b06fd212802
jeforigua <jeforigua@poligran.edu.co>
2019-10-14 22:43:39 -0500

    ajuste componente naves

--- src/app/components/naves/naves.component.ts
+++ src/app/components/naves/naves.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { SwapiService } from '../../services/swapi.service';
+
 @Component({
   selector: 'app-naves',
   templateUrl: './naves.component.html',
@@ -11,6 +12,7 @@ export class NavesComponent {
   loading: boolean;
   nextUrl: string;
   previousUrl: string;
+  
   constructor( private swapi: SwapiService) {
     this.loading = true;
     this.swapi.getCall('starships/')
