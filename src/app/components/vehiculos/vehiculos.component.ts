18237da77df2e66229b9413ea68c517972f1036f
jeforigua <jeforigua@poligran.edu.co>
2019-10-14 23:17:21 -0500

    Ajuste Componente vehiculo y html

--- src/app/components/vehiculos/vehiculos.component.ts
+++ src/app/components/vehiculos/vehiculos.component.ts
@@ -1,29 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { SwapiService } from '../../services/swapi.service';
+
 @Component({
   selector: 'app-vehiculos',
   templateUrl: './vehiculos.component.html',
   styles: []
 })
-export class VehiculosComponent implements OnInit {
+export class VehiculosComponent {
 
   vehiculos: any[] = [];
   loading: boolean;
+  nextUrl: string;
+  previousUrl: string;
+
   constructor( private swapi: SwapiService) {
     this.loading = true;
     this.swapi.getCall('vehicles/')
       .subscribe( (data: any) => {
         console.log(data);
-        this.vehiculos = data;
+        this.vehiculos = data['results'];
         this.loading = false;
+        this.nextUrl = data['next'];
+        this.previousUrl = data['previous'];
       });
   }
 
-  verVehiculo(item: any) {
-    console.log(item);
+  nextPage() {
+    if (this.nextUrl !== '') {
+      console.log(this.nextUrl);
+      this.loading = true;
+      this.swapi.getNewCall(this.nextUrl)
+        .subscribe( (data: any) => {
+          console.log(data);
+          this.vehiculos = data['results'];
+          this.nextUrl = data['next'];
+          this.previousUrl = data['previous'];
+          this.loading = false;
+        });
+    }
   }
 
-  ngOnInit() {
+  previousPage() {
+    if (this.previousUrl !== '') {
+      this.loading = true;
+      this.swapi.getNewCall(this.previousUrl)
+        .subscribe( (data: any) => {
+          console.log(data);
+          this.vehiculos = data['results'];
+          this.nextUrl = data['next'];
+          this.previousUrl = data['previous'];
+          this.loading = false;
+        });
+    }
   }
 
 }
