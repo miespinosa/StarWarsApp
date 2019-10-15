393ba679fac23a5a56b7e608ef7255a64abfd485
jeforigua <jeforigua@poligran.edu.co>
2019-10-14 22:55:01 -0500

    .

--- src/app/components/naves/naves.component.ts
+++ src/app/components/naves/naves.component.ts
@@ -15,7 +15,7 @@ export class NavesComponent {
 
   constructor( private swapi: SwapiService) {
     this.loading = true;
-    this.swapi.getCall('starships')
+    this.swapi.getCall('starships/')
       .subscribe( (data: any) => {
         console.log(data);
         this.naves = data;
