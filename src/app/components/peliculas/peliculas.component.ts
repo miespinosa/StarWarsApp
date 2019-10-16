87f2aabe1c2fb28524053134e3dcc2421f5c1220
hagomezv <arangova@hotmail.com>
2019-10-16 00:20:19 -0500

    ajustes peliculas

--- src/app/components/peliculas/peliculas.component.ts
+++ src/app/components/peliculas/peliculas.component.ts
@@ -6,12 +6,52 @@ import { SwapiService } from '../../services/swapi.service';
   templateUrl: './peliculas.component.html',
   styles: []
 })
-export class PeliculasComponent implements OnInit {
+export class PeliculasComponent  {
 
-  constructor() {
+  peliculas: any[] = [];
+  loading: boolean;
+  nextUrl: string;
+  previousUrl: string;
+
+  constructor( private swapi: SwapiService) {
+    this.loading = true;
+    this.swapi.getCall('films/')
+      .subscribe( (data: any) => {
+        console.log(data);
+        this.peliculas = data['results'];
+        this.loading = false;
+        this.nextUrl = data['next'];
+        this.previousUrl = data['previous'];
+      });
   }
 
-  ngOnInit() {
+  nextPage() {
+    if (this.nextUrl !== '' && this.nextUrl !== null) {
+      console.log(this.nextUrl);
+      this.loading = true;
+      this.swapi.getNewCall(this.nextUrl)
+        .subscribe( (data: any) => {
+          console.log(data);
+          this.peliculas = data['results'];
+          this.nextUrl = data['next'];
+          this.previousUrl = data['previous'];
+          this.loading = false;
+        });
+    }
   }
+
+  previousPage() {
+    if (this.previousUrl !== '' && this.previousUrl !== null) {
+      this.loading = true;
+      this.swapi.getNewCall(this.previousUrl)
+        .subscribe( (data: any) => {
+          console.log(data);
+          this.peliculas = data['results'];
+          this.nextUrl = data['next'];
+          this.previousUrl = data['previous'];
+          this.loading = false;
+        });
+    }
+  } 
 
 }
