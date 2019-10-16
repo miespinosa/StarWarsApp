7e3b4791332fcb47594f0e8690567c0df41c98d3
jhorman rey <rey9627@gmail.com>
2019-10-15 22:36:01 -0500

    ajustes componente especies y html

--- src/app/components/especies/especies.component.ts
+++ src/app/components/especies/especies.component.ts
@@ -1,17 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SwapiService } from '../../services/swapi.service';
 
 @Component({
   selector: 'app-especies',
   templateUrl: './especies.component.html',
   styles: []
 })
-export class EspeciesComponent implements OnInit {
+export class EspeciesComponent {
+  [x: string]: any;
 
-  constructor() { 
-    
-   }
+  naves: any[] = [];
+  loading: boolean;
+  nextUrl: string;
+  previousUrl: string;
 
-  ngOnInit() {
+  constructor( private swapi: SwapiService) {
+    this.loading = true;
+    this.swapi.getCall('species/')
+      .subscribe( (data: any) => {
+        console.log(data);
+        this.especies = data['results'];
+        this.loading = false;
+        this.nextUrl = data['next'];
+        this.previousUrl = data['previous'];
+      });
+  }
+
+  nextPage() {
+    if (this.nextUrl !== '' && this.nextUrl !== null) {
+      console.log(this.nextUrl);
+      this.loading = true;
+      this.swapi.getNewCall(this.nextUrl)
+        .subscribe( (data: any) => {
+          console.log(data);
+          this.especies = data['results'];
+          this.nextUrl = data['next'];
+          this.previousUrl = data['previous'];
+          this.loading = false;
+        });
+    }
+  }
+
+  previousPage() {
+    if (this.previousUrl !== '' && this.previousUrl !== null) {
+      this.loading = true;
+      this.swapi.getNewCall(this.previousUrl)
+        .subscribe( (data: any) => {
+          console.log(data);
+          this.especies = data['results'];
+          this.nextUrl = data['next'];
+          this.previousUrl = data['previous'];
+          this.loading = false;
+        });
+    }
   }
 
 }
